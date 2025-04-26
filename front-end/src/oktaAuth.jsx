f939c048a9b59bd9147ac4f875bc4c7373ca34b7
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2025-04-26 23:14:09 +0530

    fix

--- front-end/src/oktaAuth.jsx
+++ front-end/src/oktaAuth.jsx
@@ -1,10 +1,10 @@
 import { OktaAuth } from '@okta/okta-auth-js';
 
 const oktaAuth = new OktaAuth({
-  issuer: 'https://dev-02389312.okta.com/oauth2/default',
-  clientId: '0oaogynkmtNSmgG7V5d7',
+  issuer: 'https://dev-02389312.okta.com',
+  clientId: '0oaogzumx7dqwhRox5d7',
   redirectUri: 'http://localhost:5173/login/callback',
-  scopes: ['openid', 'profile', 'email', 'offline_access'],
+  scopes: ['openid', 'profile', 'email'],
   pkce: true, // Enable PKCE for authorization code flow
   disableHttpsCheck: true, // Disable HTTPS check for development
 });
