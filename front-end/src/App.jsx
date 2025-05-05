d2bf688e82c3fef502e7d7b7272107f1870ec10b
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2025-05-05 13:48:18 +0530

    fix

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,5 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -8,26 +12,26 @@ import Contact from "./pages/Contact";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
-import Loginbutton from "./pages/loginbutton";
 
 
 function App() {
   return (
-    <Router>
+    <>
       <Navbar />
-      
+
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About/>} /> 
-        <Route path="/contact" element={<Contact/>} />
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/signup" element={<Signup/>}/>
-        <Route path="/dashboard" element={<Dashboard/>}/>
-        <Route path="/Loginbutton" element={<Loginbutton/>}/>
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+  
+        <Route path="/loginButton" element={<LoginButton />} />
       </Routes>
       
       <Footer />
-    </Router>
+    </>
   );
 }
 
