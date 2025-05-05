9990cdbaa0dafb983189415f515a7071717fe5f1
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2025-05-05 13:48:46 +0530

    fix

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,10 +1,21 @@
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import { AuthProvider } from "@asgardeo/auth-react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+const config = {
+  signInRedirectURL: "http://localhost:5173",
+  signOutRedirectURL: "http://localhost:5173",
+  clientID: "IWS4wbk3ld7VbRptb7CFBzfh0OQa",
+  baseUrl: "https://api.asgardeo.io/t/org5rgqf",
+  scope: ["openid", "profile"],
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <AuthProvider config={config}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthProvider>
 );
